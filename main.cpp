bc4c71723b7b0d73c402b892a537a49fbf8af118
gooeyone <luszjo36@gmail.com>
2018-06-09 23:28:55 +0200

    Reading images from disk to Matrix type

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 
 #include "src/matrix.h"
 
+// TODO: extract to separate utils class
+Matrix<uint8_t> cvMat2Matrix(cv::Mat& opencv_matrix);
+
 int main(int argc, char* argv[]) {
 
     const char* DEFAULT_IMG_DIR = "input/";
     const char* img_dir;
 
-    std::vector<cv::Mat> images;
+    std::vector<Matrix<uint8_t>> images;
 
     if (argc == 2) {
         img_dir = argv[1];
@@ -25,8 +28,19 @@ int main(int argc, char* argv[]) {
     for (auto filename : filenames) {
         cv::Mat image = cv::imread(filename, 1);
         std::cout << "reading image: " << filename << std::endl;
-        images.push_back(image);
+        images.push_back(cvMat2Matrix(image));
     }
 
     return 0;
+}
+
+Matrix<uint8_t> cvMat2Matrix(cv::Mat& opencv_matrix) {
+    Shape shape(opencv_matrix.cols, opencv_matrix.rows, opencv_matrix.channels());
+    Matrix<uint8_t> matrix(shape);
+
+    for (size_t i = 0; i < shape.x * shape.y * shape.z; i++) {
+        matrix[i] = opencv_matrix.data[i];
+    }
+
+    return matrix;
 }
